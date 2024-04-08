0c3b58aa7a8d2735f2189032a556539d9d3dec4d
unknown <building.pc007@gmail.com>
2024-04-09 01:33:56 +0700

    Video 25

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,23 +2,50 @@ import React from "react";
 
 
 class DisplayInfor extends React.Component {
+
+    state = {
+        isShowListUser: true
+    };
+
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser,
+        });
+
+    };
+
+
     render() {
         const { listUsers } = this.props;
+        console.log(listUsers);
         return (
-            <div>
-                {listUsers.map((user, key) => {
-                    return (
-                        <div key={user.id}>
-                            <div>name: {user.name}</div>
-                            <div>age: {user.age}</div>
-                            <hr/>
-                        </div>
 
-                    );
+            <div>
+                <div>
+                    <br />
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShowListUser === true ? 'Hide list user' : 'Show list user'}
+                    </span>
+                </div>
+
+
+                { this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((user, key) => {
+                            // console.log(">>> Check map user: ", user);
+                            return (
+                                <div key={user.id} className={+user.age > 18 ? 'green' : 'red'} >
+                                    <div>name: {user.name}</div>
+                                    <div>age: {user.age}</div>
+                                    <hr />
+                                </div>
+
+                            );
+                        }
+                        )
+                        }
+                    </div>
                 }
-                )
-                }
-                
             </div>
         );
     }
