b0d2919f46a2a191b61e229ca561caab9a571186
PC <dark.redstar007@gmail.com>
2024-08-21 20:03:33 +0700

    video 80

--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './Layout';
 import { BrowserRouter } from 'react-router-dom';
-
 import 'nprogress/nprogress.css';
 
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <Provider store={store}>
-    {/*<React.StrictMode>*/}
+    <PersistGate loading={null} persistor={persistor}>
 
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+      {/*<React.StrictMode>*/}
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+      {/*</React.StrictMode>*/}
 
-    {/*</React.StrictMode>*/}
+    </PersistGate>
+
   </Provider>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
